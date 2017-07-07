b52fb39e6e9aa8303532e2e0c4c0e72659478735
Sam Moss <sam.moss321@gmail.com>
2017-07-06 21:42:30 -0500

    finishes form and checks for template integrity

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -4,9 +4,18 @@ class GreetingsController < ApplicationController
 
   def new
     @greeting = Greeting.new
+    @default_template = Greeting::DEFAULT_TEMPLATE.to_s
   end
 
   def display
+    @greeting = Greeting.new(greeting_params)
+    template_diff = @greeting.check_template
+    unless template_diff.empty?
+      flash[:notice] = "Custom template must contain #{template_diff}"
+      redirect_back fallback_location: new_greeting_path
+      return
+    end
+    @processed_greeting = @greeting.process
   end
 
   private
@@ -28,5 +37,9 @@ class GreetingsController < ApplicationController
 
   def find_file(file_name)
     JSON.parse(File.read(File.join(Rails.root, 'docs', "#{file_name}.json")))
+  end
+
+  def greeting_params
+    params.permit(:company, :guest, :template)
   end
 end
--- app/models/greeting.rb
+++ app/models/greeting.rb
@@ -1,16 +1,21 @@
 class Greeting
    include ActiveModel::Model
-   attr_accessor :company, :guest
-   DEFAULT_GREETING = 'time name, and welcome to location! room is now ready for you. \
-  Enjoy your stay and let us know if you need anything!'.freeze
+   attr_accessor :company, :guest, :template
+   DEFAULT_TEMPLATE = 'time name, and welcome to location! room is now ready for you. Enjoy your stay and let us know if you need anything!'.freeze
 
-  def get_time
-  end
+    def get_time
+    end
 
-  def compose_greeting
-  end
+    def compose_greeting
 
-  def check_greeting
-  end
+    end
+
+    #Checks the template for all the template pieces.
+    def check_template
+      template_array = %w[time name location room]
+      regex_union = Regexp.union(template_array)
+      contents_array = template.scan(regex_union)
+      diff_array = template_array - contents_array
+    end
 
 end
